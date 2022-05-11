ea708db83583b546ce0a480ba3a49a5589a19fa6
Zifeng Wang <zifengw2@illinois.edu>
2022-05-11 13:10:40 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
+import os
 import setuptools
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r") as f:
     long_description = f.read()
+
+# read the contents of requirements.txt
+with open(os.path.join(this_directory, 'requirements.txt'),
+          encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
 setuptools.setup(
     name = 'transtab',
     version = '0.0.1',
     author = 'Zifeng Wang',
     author_email = 'zifengw2@illinois.edu',
-    description = 'A flexible tabular prediction model that handles variable input tables.',
+    description = 'A flexible tabular prediction model that handles heterogeneous input tables.',
     url = 'https://github.com/RyanWangZf/transtab',
+    keywords=['tabular prediction'],
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(exclude=['test']),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
