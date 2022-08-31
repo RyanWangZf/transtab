d08129e65e1f4c6b54e2250a815e9e2138e85df6
ryanwangzf <1399320648@qq.com>
2022-08-31 14:53:48 -0500

    update doc release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Zifeng Wang'
 author = 'Zifeng Wang'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
