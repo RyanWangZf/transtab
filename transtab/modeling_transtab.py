5e2f04c49e7aaf82702f2af63e9ae8697e5ba4a7
SOVIETIC-BOSS88 <surhar88@hotmail.com>
2023-05-04 16:10:01 +0200

    add linear_regressor

--- transtab/modeling_transtab.py
+++ transtab/modeling_transtab.py
@@ -588,6 +588,22 @@ class TransTabLinearClassifier(nn.Module):
         logits = self.fc(x)
         return logits
 
+
+class TransTabLinearRegressor(nn.Module):
+    def __init__(self,
+        hidden_dim=128) -> None:
+        super().__init__()
+        self.fc = nn.Linear(hidden_dim, 1)
+        self.norm = nn.LayerNorm(hidden_dim)
+
+    def forward(self, x) -> Tensor:
+        x = x[:,0,:] # take the cls token embedding
+        x = self.norm(x)
+        prediction = self.fc(x)
+        return prediction
+
+
+
 class TransTabProjectionHead(nn.Module):
     def __init__(self,
         hidden_dim=128,
