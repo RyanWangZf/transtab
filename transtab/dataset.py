3a5c09513591780aade8b1c60ce2ad2c70e99527
SOVIETIC-BOSS88 <surhar88@hotmail.com>
2023-05-04 16:56:18 +0200

    add shuffle to load_single and load_data

--- transtab/dataset.py
+++ transtab/dataset.py
@@ -20,7 +20,7 @@ OPENML_DATACONFIG = {
     'credit-g': {'bin': ['own_telephone', 'foreign_worker']},
 }
 
-def load_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, seed=123):
+def load_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, seed=123, shuffle=True):
     '''Load datasets from the local device or from openml.datasets.
 
     Parameters
@@ -68,7 +68,7 @@ def load_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, se
     if dataset_config is None: dataset_config = OPENML_DATACONFIG
     if isinstance(dataname, str):
         # load a single tabular data
-        return load_single_data(dataname=dataname, dataset_config=dataset_config, encode_cat=encode_cat, data_cut=data_cut, seed=seed)
+        return load_single_data(dataname=dataname, dataset_config=dataset_config, encode_cat=encode_cat, data_cut=data_cut, seed=seed, shuffle=shuffle)
     
     if isinstance(dataname, list):
         # load a list of datasets, combine together and outputs
@@ -77,7 +77,7 @@ def load_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, se
         train_list, val_list, test_list = [], [], []
         for dataname_ in dataname:
             allset, trainset, valset, testset, cat_cols, num_cols, bin_cols = \
-                load_single_data(dataname_, dataset_config=dataset_config, encode_cat=encode_cat, data_cut=data_cut, seed=seed)
+                load_single_data(dataname_, dataset_config=dataset_config, encode_cat=encode_cat, data_cut=data_cut, seed=seed, shuffle=shuffle)
             num_col_list.extend(num_cols)
             cat_col_list.extend(cat_cols)
             bin_col_list.extend(bin_cols)
@@ -87,7 +87,7 @@ def load_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, se
             test_list.append(testset)
         return all_list, train_list, val_list, test_list, cat_col_list, num_col_list, bin_col_list
 
-def load_single_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, seed=123):
+def load_single_data(dataname, dataset_config=None, encode_cat=False, data_cut=None, seed=123, shuffle=True):
     '''Load tabular dataset from local or from openml public database.
     args:
         dataname: Can either be the data directory on `./data/{dataname}` or the dataname which can be found from the openml database.
