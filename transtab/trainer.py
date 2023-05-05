fd4b8c6d8b717e57d588fd736c85198717a410c2
SOVIETIC-BOSS88 <surhar88@hotmail.com>
2023-05-05 11:15:32 +0200

    add prints for classification

--- transtab/trainer.py
+++ transtab/trainer.py
@@ -123,6 +123,9 @@ class Trainer:
                 for dataindex in range(len(self.trainloader_list)):
                     for data in self.trainloader_list[dataindex]:
                         self.optimizer.zero_grad()
+                        print(data[0], data[1])
+                        print(type(data[0]), type(data[1]))
+                        print(len(data[0]),  len(data[1]))
                         logits, loss = self.model(data[0], data[1]) ##todo
                         loss.backward()
                         self.optimizer.step()
