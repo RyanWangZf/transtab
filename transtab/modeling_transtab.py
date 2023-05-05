ff7d00a26f244de4137c168ab4ec060ae501b3df
SOVIETIC-BOSS88 <surhar88@hotmail.com>
2023-05-05 11:29:28 +0200

    print the forward

--- transtab/modeling_transtab.py
+++ transtab/modeling_transtab.py
@@ -579,6 +579,7 @@ class TransTabLinearRegressor(nn.Module):
         self.norm = nn.LayerNorm(hidden_dim)
 
     def forward(self, x) -> Tensor:
+        print(x[:,0,:], type(x[:,0,:]), len(x[:,0,:]))
         x = x[:,0,:] # take the cls token embedding
         x = self.norm(x)
         prediction = self.fc(x)
