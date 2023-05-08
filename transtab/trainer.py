7d46471c33c8e90b1232a28dc1cacf3356a06bdc
SOVIETIC-BOSS88 <surhar88@hotmail.com>
2023-05-08 15:51:52 +0200

    add print for evaluate

--- transtab/trainer.py
+++ transtab/trainer.py
@@ -199,6 +199,7 @@ class Trainer:
                         loss_list.append(loss.item())
                     if logits is not None: ##todo
                         if logits.shape[-1] == 1: # binary classification
+                            print(pred_list)
                             pred_list.append(logits.sigmoid().detach().cpu().numpy())
                             print('binary') #todo
                         else: # multi-class classification
