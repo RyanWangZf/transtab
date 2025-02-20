cd8c65413c8c0851b0dc0938fa31205ce2ae206d
Zifeng <zifengw2@illinois.edu>
2025-02-19 23:22:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(this_directory, 'requirements.txt'),
 
 setuptools.setup(
     name = 'transtab',
-    version = '0.0.5',
+    version = '0.0.6',
     author = 'Zifeng Wang',
     author_email = 'zifengw2@illinois.edu',
     description = 'A flexible tabular prediction model that handles variable-column input tables.',
